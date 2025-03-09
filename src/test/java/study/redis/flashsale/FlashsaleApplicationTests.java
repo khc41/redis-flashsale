65dba25024c4212abe0d8beeaee71c933a8fa7cc
heechul <khc4190@naver.com>
2025-03-09 14:14:48 +0900

    project init

---
+++ src/test/java/study/redis/flashsale/FlashsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package study.redis.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
